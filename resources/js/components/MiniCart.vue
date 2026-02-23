bfd21c37aebfcc808a5162770fa4e0565c6bfc3c
Yurii Rybin <jura0633728096@gmail.com>
2026-02-23 19:28:21 +0200

    front| fix currency in cart

--- resources/js/components/MiniCart.vue
+++ resources/js/components/MiniCart.vue
@@ -6,7 +6,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
             <span class="font-bold">{{ cartStore.totalItems }} шт.</span>
-            <span v-if="cartStore.totalItems > 0">({{ Number(cartStore.totalPrice).toFixed(2) }} руб.)</span>
+            <span v-if="cartStore.totalItems > 0">({{ Number(cartStore.totalPrice).toFixed(2) }} $.)</span>
         </a>
 
         <transition
@@ -23,7 +23,7 @@
                     <div v-for="item in cartStore.cartItemsArray" :key="item.id" class="flex justify-between items-center border-b pb-2">
                         <div>
                             <p class="text-sm font-semibold">{{ item.name }}</p>
-                            <p class="text-xs text-gray-500">{{ item.quantity }} x {{ Number(item.price).toFixed(2) }} руб.</p>
+                            <p class="text-xs text-gray-500">{{ item.quantity }} x {{ Number(item.price).toFixed(2) }} $.</p>
                         </div>
                         <button @click="cartStore.removeFromCart(item.id)" class="text-red-500 text-sm hover:underline" title="Удалить">
                             ✕
